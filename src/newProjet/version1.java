aaf1ace8b7889f09a7bb4f8dbfe6eb2231768a6d
AnushaGiri417 <anushagiri417@gmail.com>
2021-06-18 12:07:05 -0400

    first commit for version1

---
+++ src/newProjet/version1.java
@@ -0,0 +1,16 @@
+package newProjet;
+
+public class version1 {
+    public static void main(String[] args) {
+
+        Boolean isHungry = true;
+
+        if(isHungry){
+            System.out.println("Eat something");
+        }else {
+            System.out.println("Keep working");
+        }
+
+
+    }
+    }
